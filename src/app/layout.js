6a3c0b66c7dae248d6f4804a10eff3c6215b1179
Arvin Poole <github@opensesa.me>
2026-05-17 23:28:00 -0500

    Added production GTM container

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXXXXX');
+              })(window,document,'script','dataLayer','GTM-MPF8P92');
             `,
           }}
         />
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body className="bg-zinc-950 text-zinc-300 font-sans antialiased">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MPF8P92"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
